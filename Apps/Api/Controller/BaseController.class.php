80d14c158c9da6f97e162ae5738cc7f93614e2f3
NENER <714571611@qq.com>
2014-10-28 13:21:18 +0800

    修改api
    增加状态表示：-1：未登录，-2：没有api权限

--- Apps/Api/Controller/BaseController.class.php
+++ Apps/Api/Controller/BaseController.class.php
@@ -16,17 +16,23 @@ class BaseController extends Controller {
 	 * api key 检测
 	 */
 	public function _initialize() {
-		$arr = file_get_contents ( "php://input" );
-		$arr = json_decode ( $arr, true );
-		if (! $arr) {
-			$arr = I ( 'param.' );
+		$appkey = I ( 'get.APPKEY' );
+		if (! $appkey) {
+			$appkey = I ( 'post.APPKEY' );
 		}
-		$key = $arr ['APPKEY'];
+		if (! $appkey) {
+			
+			$arr = file_get_contents ( "php://input" );
+			$arr = json_decode ( $arr, true );
+			$key = $arr ['APPKEY'];
+		} else {
+			$key = $appkey;
+		}		
 		$appkey = new appkeyModel ();
 		if (! $appkey->checkkey ( $key )) {
 			echo json_encode ( array (
-					'status' => 0,
-					'msg' => '没有API访问权限！'
+					'status' => -2,
+					'msg' => '没有API访问权限！' 
 			) );
 			exit ();
 		}
