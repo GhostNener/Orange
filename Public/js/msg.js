1d6806ef790ae8d76d610774a3c2837600009ebf
NENER <714571611@qq.com>
2014-11-02 12:53:06 +0800

    格式化js

--- Public/js/msg.js
+++ Public/js/msg.js
@@ -1,18 +1,18 @@
+/*errormsg successmsg   msgbox*/
+function alertmsg(id, msg, intime, outtime) {
+    id = '#' + id;
+    $(id).stop();
+    $(id).children('.msgbox').html(msg);
+    $(id).fadeIn(intime);
+    setTimeout(function() {
+        $(id).fadeOut(outtime);
+    },
+    outtime);
 
-	/*errormsg successmsg   msgbox*/
-	function alertmsg(id,msg,intime,outtime){
-			id='#'+id;
-			$(id).stop();
-			$(id).children('.msgbox').html(msg);
-			$(id).fadeIn(intime);
-			setTimeout(function(){
-				$(id).fadeOut(outtime);
-			},outtime);
-			
-		}
-	function showerrormsg(msg,intime,outtime){
-		alertmsg('errormsg',msg,intime,outtime);
-		}
-	function showsuccessmsg(msg,intime,outtime){
-			alertmsg('successmsg',msg,intime,outtime);
-		}
+}
+function showerrormsg(msg, intime, outtime) {
+    alertmsg('errormsg', msg, intime, outtime);
+}
+function showsuccessmsg(msg, intime, outtime) {
+    alertmsg('successmsg', msg, intime, outtime);
+}
