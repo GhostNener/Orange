2ab38318745eeb4b7b4b337969703e38276f07e2
QingWei-Li <leecinwell@gmail.com>
2014-11-05 19:49:15 +0800

    后台显示用户名

--- Apps/Admin/Controller/BaseController.class.php
+++ Apps/Admin/Controller/BaseController.class.php
@@ -4,6 +4,8 @@ namespace Admin\Controller;
 
 use Think\Controller;
 use Usercenter\Model\userModel;
+use Usercenter\Model\view_user_info_avatarModel;
+
 /**
  * 后台首页
  *
@@ -19,6 +21,17 @@ class BaseController extends Controller {
 		$rst=$model->islogin(null,true,false);
 		if(!$rst){
 			redirect(U('Usercenter/User/index',array('isadmin'=>true)));
+		}
+		else{
+			$usermodel = null;
+			$m = new view_user_info_avatarModel ();
+			$usermodel = $m->getinfo ();
+			if ($usermodel ['status'] == 1) {
+				$usermodel = $usermodel ['msg'];
+			} else {
+				$usermodel = null;
+			}
+			$this->assign ( 'usermodel', $usermodel );
 		}
 	}
 }
