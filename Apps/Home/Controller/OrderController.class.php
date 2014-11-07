1ee549bde4588f5c512e418986127ccd29c18848
NENER <714571611@qq.com>
2014-11-07 11:30:47 +0800

    增加订单模板

--- Apps/Home/Controller/OrderController.class.php
+++ Apps/Home/Controller/OrderController.class.php
@@ -23,16 +23,25 @@ class OrderController extends BaseController {
 			die ();
 		}
 		$m = new goodsModel ();
+
+		/*获得商品模型  */
 		$goods = $m->findone ( $Id );
 		if (! $goods) {
 			$this->error ( '商品不存在或已下架' );
 			die ();
 		}
+		/*订单code  */
+		$arrcode=array('code'=>date('YmdHis').$goods['Id'],'time'=>time());
 		$m = new user_addressModel ();
 		/*获得地址  */
 		$alist = $m->getbyid ( cookie ( '_uid' ) );
 		/*获得可用的交易方式  */
 		$waylist = $this->createtradeway ( $goods ['TradeWay'] );
+		$this->assign('ordermodel',$arrcode);
+		$this->assign('goods',$goods);
+		$this->assign('tradewaylist',$waylist);
+		$this->assign('useraddress',$alist);
+		$this->display();
 	}
 	/**
 	 * 创建可用的交易方式
@@ -44,24 +53,28 @@ class OrderController extends BaseController {
 		switch ($id) {
 			case 1 :
 				$arr [] = array (
-						'Id' => $id,
-						'msg' => '线上' 
+						'id' => $id,
+						'txt' => '线上' ,
+						'msg'=>'用“金橘”作为货币交易,确定后“金橘”将被冻结, 直到确认收货后付款给卖家'
 				);
 				break;
 			case 2 :
 				$arr [] = array (
-						'Id' => $id,
-						'msg' => '线下' 
+						'id' => $id,
+						'txt' => '线下' ,
+						'msg'=>'线下联系卖家, 按显示的价格进行交易'
 				);
 				break;
 			case 3 :
 				$arr [] = array (
-						'Id' => 2,
-						'msg' => '线下' 
+						'id' => 1,
+						'txt' => '线上',
+						'msg'=>'用“金橘”作为货币交易,确定后“金橘”将被冻结, 直到确认收货后付款给卖家'
 				);
 				$arr [] = array (
-						'Id' => 1,
-						'msg' => '线下' 
+						'id' => 2,
+						'txt' => '线下' ,
+						'msg'=>'线下联系卖家, 按显示的价格进行交易'
 				);
 				break;
 			default :
