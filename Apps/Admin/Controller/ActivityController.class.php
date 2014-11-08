fce11f019456ea15b81cfffabcd79c39e53af0f1
QingWei-Li <leecinwell@gmail.com>
2014-11-08 20:53:50 +0800

    富文本插件

--- Apps/Admin/Controller/ActivityController.class.php
+++ Apps/Admin/Controller/ActivityController.class.php
@@ -10,6 +10,27 @@ namespace Admin\Controller;
  */
 class ActivityController extends BaseController {
 	public function index() {
+			$model = M ( 'activity' );
+		// 查询条件
+		$wherrArr = array (
+				'Status' => 10 
+		);
+		// 总数
+		$allCount = $model->where ( $wherrArr )->count ();
+		// 分页
+		$Page = new \Think\Page ( $allCount, 10 );
+		
+		$showPage = $Page->show ();
+		// 分页查询
+		$list = $model->where ( $wherrArr )->limit ( $Page->firstRow . ',' . $Page->listRows )->select ();
+
+		$this->assign ( 'list', $list );
+		$this->assign ( 'page', $showPage );
 		$this->display ();
+	}
+
+	public function save()
+	{
+		# code...
 	}
 }
