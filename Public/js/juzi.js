9599b9b068e9ad934d944e6839b373842864980f
QingWei-Li <leecinwell@gmail.com>
2014-11-10 13:54:28 +0800

    忽略用户下拉列表的active

--- Public/js/juzi.js
+++ Public/js/juzi.js
@@ -35,6 +35,9 @@ $(document).ready(function() {
 	/*导航栏切换*/
 	$(".navbar-nav").find("a").each(function(){
 		if (window.location.href.toLocaleLowerCase().indexOf($(this).attr("href").replace(".html","").toLocaleLowerCase(),1)>0) {
+			//忽略掉用户下拉列表的响应
+			if($(this).parent().parent().attr('class')=='dropdown-menu')return;
+			
 			$(".navbar-nav").children().removeClass("active");
 			$(this).parent().addClass("active");
 			return false;
