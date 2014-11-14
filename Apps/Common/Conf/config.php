201119a72688ab43d3517adc58368e070f066e43
cinwell <leecinwell@gmail.com>
2014-11-14 10:14:38 +0800

    消息提醒优化

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -144,10 +144,10 @@ return array (
 						'jpeg' 
 				),
 				'driverConfig' => array (
-						'secrectKey' => 'ApFyYUL4aaaPbGAYQRJT1-4qGUJfjziQ7KQ_SJni',//'-LdkxeAxW_or_1UssZbSdATJmlVZm5G-M4oWRDcD',
-						'accessKey' => 'dU131Y7XxO1dZtrPNWFg2RCW1PbemyoVPecdldP_',//'fJOIxQXMh6cn0j0FNSsx4uSEwG9sFCkel0BhwdOw',
-						'domain' => 'ghostdatabase.qiniudn.com',
-						'bucket' => 'ghostdatabase',//'bigoranger',
+						'secrectKey' => '-LdkxeAxW_or_1UssZbSdATJmlVZm5G-M4oWRDcD',
+						'accessKey' => 'fJOIxQXMh6cn0j0FNSsx4uSEwG9sFCkel0BhwdOw',
+						'domain' => 'bigoranger.qiniudn.com',
+						'bucket' => 'bigoranger',
 						'CallbackUrl' => '',
 						'CallbackBody' => 'key=$(key)&cid=$(x:cid)&sid=$(x:sid)&ckey=$(x:ckey)',
 						'Expires' => 36000 
