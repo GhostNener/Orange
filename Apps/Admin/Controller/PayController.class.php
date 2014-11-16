40ac171f01e7b70787a378ff3c13ad0aaf14854c
cinwell <leecinwell@gmail.com>
2014-11-16 18:10:53 +0800

    支付宝后台界面完成

--- Apps/Admin/Controller/PayController.class.php
+++ Apps/Admin/Controller/PayController.class.php
@@ -10,7 +10,11 @@ namespace Admin\Controller;
  */
 class PayController extends BaseController {
 	public function index() {
-
+		//https://consumeprod.alipay.com/record/advanced.htm?beginDate=2014.08.16&beginTime=00%3A00&endDate=2014.11.16&endTime=24%3A00&dateRange=threeMonths&status=all&keyword=bizOutNo&keyValue=&dateType=createDate&minAmount=&maxAmount=&fundFlow=in&tradeModes=FP&tradeType=tranAlipay&categoryId=&_input_charset=utf-8
 		$this->display ();
+	}
+
+	public function setting() {
+		$this->display();
 	}
 }
