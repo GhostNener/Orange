a466005fe2b5d2ef0f49a8682e2bc956b8e1e3af
cinwell <leecinwell@gmail.com>
2014-11-16 17:39:08 +0800

    后台支付宝功能

---
+++ Apps/Admin/Controller/PayController.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Admin\Controller;
+
+/**
+ * 后台首页
+ *
+ * @author Cinwell
+ *        
+ */
+class PayController extends BaseController {
+	public function index() {
+
+		$this->display ();
+	}
+}
