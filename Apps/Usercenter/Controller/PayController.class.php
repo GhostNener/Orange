7c718c0c1ad3da45b94e9eb6b61721527a8b0850
cinwell <leecinwell@gmail.com>
2014-11-18 10:54:15 +0800

    完善充值页面用户体验

--- Apps/Usercenter/Controller/PayController.class.php
+++ Apps/Usercenter/Controller/PayController.class.php
@@ -17,7 +17,21 @@ class PayController extends LoginController {
 		$this->display();
 	}
 
+	public function check() {
+		$code = I('code');
+
+		$verify = new \Think\Verify ();
+		$verify->reset = true;
+		$rst = $verify->check ( $code, '' );
+		if($rst){
+			$this->success('验证码正确');
+		}else{
+			$this->error('验证码错误');
+		}
+	}
+
 	public function tradeno(){
+		
 		$this->success("充值成功，共充值50元，请到个人中心核对", U('/Usercenter/Index/index'));
 	}
 }
