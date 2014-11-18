69f41f81637b7b4966c1519d3498c20e0e92f470
cinwell <leecinwell@gmail.com>
2014-11-18 13:28:58 +0800

    后台商品管理

--- Apps/Admin/Controller/GoodsController.class.php
+++ Apps/Admin/Controller/GoodsController.class.php
@@ -31,4 +31,54 @@ class GoodsController extends BaseController {
 		$this->display ();
 	}
 
+	public function update() {
+		$id = ( int ) I ( 'get.Id' );
+		if (! $id) {
+			$this->error ( "页面不存在" );
+		}
+		$whereArr = array (
+				'Id' => $id 
+		);
+		$dal = M ();
+		$dal->startTrans (); // 开始事务
+		$model = M ( 'goods' );
+		$model->Status = I('get.status');
+		$r1 = $model->where ( $whereArr )->save (); // 操作1
+		
+		if ($r1) { // 成功
+			$dal->commit (); // 提交事务
+			$this->success ( "操作成功" );
+			
+		} else {
+			$dal->rollback (); // 否则回滚
+			$this->error ( "操作失败" );
+		}
+	}
+
+	public function clear() {
+		
+	}
+
+	public function recycle()
+	{
+		$model = M ( 'view_goods_list' );
+		// 查询条件
+		$wherrArr = array (
+				'Status' => 70
+		);
+		
+		// 总数
+		$allCount = $model->where ( $wherrArr )->count ();
+		// 分页
+		$Page = new \Think\Page ( $allCount, 10 );
+		
+		$showPage = $Page->show ();
+		// 分页查询
+		$list = $model->where ( $wherrArr )->order('CreateTime desc')->limit ( $Page->firstRow . ',' . $Page->listRows )->select ();
+
+		$this->assign ( 'list', $list );
+		$this->assign ( 'page', $showPage );
+		$this->display ();
+	}
+
 }
--- Apps/Admin/Controller/PayController.class.php
+++ Apps/Admin/Controller/PayController.class.php
@@ -12,7 +12,6 @@ require_once 'ORG/AES/AES.php';
 class PayController extends BaseController {
 
 	public function index() {
-		//https://consumeprod.alipay.com/record/advanced.htm?beginDate=2014.08.16&beginTime=00%3A00&endDate=2014.11.16&endTime=24%3A00&dateRange=threeMonths&status=all&keyword=bizOutNo&keyValue=&dateType=createDate&minAmount=&maxAmount=&fundFlow=in&tradeModes=FP&tradeType=tranAlipay&categoryId=&_input_charset=utf-8
 
 		$model = M('alipay');
 		$list = $model->select();
